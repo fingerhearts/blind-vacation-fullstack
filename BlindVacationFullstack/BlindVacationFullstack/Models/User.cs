4e1eed54090d94094daad63ca135be94635e7db6
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-12 10:43:15 -0800

    Seeded database, first database migration succesfull

---
+++ BlindVacationFullstack/BlindVacationFullstack/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlindVacationFullstack.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Color FaveColor { get; set; }
+        public enum Color
+        {
+            Yellow = 1,
+            Red,
+            Purple,
+            Cyan,
+            Black,
+            Brown,
+            Green,
+            Turquoise,
+            Orange,
+            Pink,
+            White,
+            Ivory,
+            Blue,
+            Velvet,
+            Gold,
+            Platinum
+        }
+
+        //Nav Properties
+
+        public ICollection<Trip> Trips { get; set; }
+
+    }
+}
