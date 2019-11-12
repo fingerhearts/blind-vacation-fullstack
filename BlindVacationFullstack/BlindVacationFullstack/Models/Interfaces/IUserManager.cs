b80fea24d50fda46598dbe3161d57fbdbfda897f
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-12 11:48:26 -0800

    coded out user crud ops

--- BlindVacationFullstack/BlindVacationFullstack/Models/Interfaces/IUserManager.cs
+++ BlindVacationFullstack/BlindVacationFullstack/Models/Interfaces/IUserManager.cs
@@ -1,7 +1,15 @@
-﻿namespace BlindVacationFullstack.Models.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlindVacationFullstack.Models.Interfaces
 {
     public interface IUserManager
     {
+        Task CreateUser(User user);
+        Task<User> GetUser(int userid);
+        Task UpdateUser(User user);
+        Task DeleteUser(int userid);
+        Task<IEnumerable<Trip>> GetSavedTrips(int userid);
 
     }
 }
--- BlindVacationFullstack/BlindVacationFullstack/Models/Services/UserService.cs
+++ BlindVacationFullstack/BlindVacationFullstack/Models/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using BlindVacationFullstack.Data;
 using BlindVacationFullstack.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,35 @@ namespace BlindVacationFullstack.Models.Services
         public UserService(VacationMVCDbContext context)
         {
             _context = context;
+        }
+
+        public async Task CreateUser(User user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUser(int userid)
+        {
+            _context.Remove(await GetUser(userid));
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Trip>> GetSavedTrips(int userid)
+        {
+            var trips = await _context.Trips.Where(x => x.UserID == userid).ToListAsync();
+            return trips;
+        }
+
+        public async Task<User> GetUser(int userid)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.ID == userid);
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
         }
     }
 }
