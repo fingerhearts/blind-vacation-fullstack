207c5e6f2e2c3a3189393304e40f04ca8d255759
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-12 09:52:28 -0800

    Added user secrets and dbcontect file

---
+++ BlindVacationFullstack/BlindVacationFullstack/Data/VacationMVCDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlindVacationFullstack.Data
+{
+    public class VacationMVCDbContext : DbContext
+    {
+        public VacationMVCDbContext(DbContextOptions<VacationMVCDbContext> options)
+        {
+
+        }
+    }
+}
--- BlindVacationFullstack/BlindVacationFullstack/Startup.cs
+++ BlindVacationFullstack/BlindVacationFullstack/Startup.cs
@@ -2,21 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlindVacationFullstack.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BlindVacationFullstack
 {
     public class Startup
+
     {
+        public IConfiguration Configuration { get; }
+        public IHostEnvironment Environment { get; }
+
+        public Startup(IHostEnvironment environment)
+        {
+            Environment = environment;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string connString = Environment.IsDevelopment()
+                ? Configuration["ConnectionStrings:DefaultConnection"]
+                : Configuration["ConnectionStrings:ProductionConnection"];
+
+            services.AddDbContext<VacationMVCDbContext>(options =>
+            options.UseSqlServer(connString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
